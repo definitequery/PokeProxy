0be896bb21695c99a350b00d8fb88cdb90a782bc
Aaron Simpson <aaron@aaronsimpson.org>
2024-11-09 19:44:02 -0600

    Adding env handling

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PokeProxy 
 An API proxy built in Express atop the PokeAPI.
+
+## Usage
+1. Clone the repo
+2. Navigate to repo directory `cd pokeproxy`
+3. Run `npm ci`
+4. Create a `.env` with the following values
+```
+POKEMON_API_URL='https://pokeapi.co/api/v2'
+```
+5. Start the app with `npm start`
--- api_service.js
+++ api_service.js
@@ -1,5 +1,7 @@
+import 'dotenv/config'
+
 export async function get(resource, param) {
-  const response = await fetch(`https://pokeapi.co/api/v2/${resource}/${param}`);
+  const response = await fetch(`${process.env.POKEMON_API_URL}/${resource}/${param}`);
   const json = await response.json();
   return json;
 }
